5da12b39431afd97ceb393db5456758cf0ec6568
unknown <ajitpatil1202@gmail.com>
2014-11-16 12:57:34 +0530

    new scene and pc movements added

---
+++ Assets/Scripts/MoveCS.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCS : MonoBehaviour {
+	public float movementSpeed = 1;
+	public float turningSpeed = 60;
+	// Update is called once per frame
+	void Update () {
+		//float horizontal = Input.GetAxis("Horizontal") * turningSpeed * Time.deltaTime;
+		//transform.Rotate(0, horizontal, 0);
+		
+		float vertical = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
+		transform.Translate(0, 0, vertical);
+		//transform.position = transform.position + Camera.main.transform.forward * movementSpeed * Time.deltaTime;
+	}
+}
