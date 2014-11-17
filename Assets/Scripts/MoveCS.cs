1bf1a41e961dd0b213dfea1241d883e3c93ccd35
unknown <ajitpatil1202@gmail.com>
2014-11-17 18:57:25 +0530

    First github commit

--- Assets/Scripts/MoveCS.cs
+++ Assets/Scripts/MoveCS.cs
@@ -4,13 +4,13 @@ using System.Collections;
 public class MoveCS : MonoBehaviour {
 	public float movementSpeed = 1;
 	public float turningSpeed = 60;
+	//public Vector3 face;
 	// Update is called once per frame
 	void Update () {
 		//float horizontal = Input.GetAxis("Horizontal") * turningSpeed * Time.deltaTime;
 		//transform.Rotate(0, horizontal, 0);
-		
-		float vertical = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
-		transform.Translate(0, 0, vertical);
-		//transform.position = transform.position + Camera.main.transform.forward * movementSpeed * Time.deltaTime;
+		//float vertical = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
+		//transform.Translate(0, 0, vertical);
+		transform.position += Vector3.forward * Time.deltaTime * movementSpeed;
 	}
 }
