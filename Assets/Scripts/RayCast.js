792a0f34280476477a50805d4adeb1bf3b15eb2d
unknown <ajitpatil1202@gmail.com>
2014-12-05 01:09:49 +0530

    GUI working in Dive camera

--- Assets/Scripts/RayCast.js
+++ Assets/Scripts/RayCast.js
@@ -6,9 +6,12 @@
 		var hit : RaycastHit;
 		var Bullseye : GameObject;
 		if (Physics.Raycast (ray, hit))	{
+			//Debug code
+			Debug.DrawLine (ray.origin, hit.point);
 			Bullseye = GameObject.Find("Bullseye");
 			Bullseye.transform.position.x = hit.point.x;
 			Bullseye.transform.position.y = hit.point.y;
 			Bullseye.transform.position.z = hit.point.z;
+			
 		}	
 	}
