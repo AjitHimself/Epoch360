0af51d3b87fd7d4a711947ca1d967a8916c07aa1
unknown <ajitpatil1202@gmail.com>
2014-12-11 18:10:21 +0530

    Welcome screen added

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+	public GameObject Panel1;
+	void Start () {
+		Panel1 = GameObject.Find ("Panel1");
+	}
+	void Update () {
+
+	}
+	void OnClick(){
+		Debug.Log ("IM CLICKED");
+		NGUITools.SetActive (Panel1, false);
+	}
+}
